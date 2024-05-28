b206c2d818cf96343f14f739fbb0e10fba2cd362
Ashoqullah <as.coder465@gmail.com>
2024-05-28 07:22:41 +0430

    first version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b4bfd459865a8d636f26aca0d330ae297c006c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4150529945"
+    serviceWorkerVersion: "2690464617"
   }
 });
